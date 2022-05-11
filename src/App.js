8419580b9aede9b02c45468744bf77cde7ccbc42
rubrym <79154930+rubrym@users.noreply.github.com>
2022-05-11 22:12:53 +0200

    Commit de actualizacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/styles.css";
+import logo from "../src/img/logo.png";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Mi proyecto web CSS</title>
+      </head>
+      <body>
+        inicio de la cabecera
+        <header id="header">
+          <div id="logotipo">
+            <img src={logo} alt="logotipo"></img>
+          </div>
+          <h1>
+            <a href="app.js">MASTER EN CSS</a>
+          </h1>
+        </header>
+        fin de la cabecera
+      </body>
     </div>
   );
 }
